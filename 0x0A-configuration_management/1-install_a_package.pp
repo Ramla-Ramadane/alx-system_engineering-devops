988251fd5912d5525085d0a7c961bc5129570a50
Ramla-Ramadane <ramlaramadane@gmail.com>
2024-03-22 03:07:13 -0700

    C management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install puppet-lint version (2.1.0)
-package {'puppet-lint':
+# Install  version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
