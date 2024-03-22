8524d1c845248307ea6e76ce67f88a56c4140beb
Ramla-Ramadane <ramlaramadane@gmail.com>
2024-03-22 03:29:42 -0700

    C management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #install puppet-lint
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin'],
+  refreshonly => true,
 }
