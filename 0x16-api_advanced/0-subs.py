3abc84f103788d658fefc0d00a0b936734ccd9e6
Ramla-Ramadane <ramlaramadane@gmail.com>
2024-05-07 03:26:33 -0700

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,8 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    if response.status_code == 200:
+        data = response.json().get("data")
+        if data:
+            return results.get("subscribers", 0)
+    return 0
